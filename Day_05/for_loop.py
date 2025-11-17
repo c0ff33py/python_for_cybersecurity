9e921f39f87eaafaf891b55e6d0177203ab7f679
c0ff33py <c0ff33py@gmail.com>
2025-11-17 13:15:06 +0630

    for loop

---
+++ Day_05/for_loop.py
@@ -0,0 +1,15 @@
+# Example of a "for" loop with the keyword and the "range()" function:
+
+for number in range(1, 6+1, 2):
+    print(number * 3)
+
+'''The range() function
+The range() function generates a sequence of numbers. code uses range(1, 6+1, 2) which simplifies to range(1,7,2).
+This function takes three arguments: a strat value, a stop value and a step value.
+start value: 1
+stop value: 7
+step value: 2
+so, between 1 and 7 value must be 1, 3, 5 
+
+it will print 3,9,5
+'''
